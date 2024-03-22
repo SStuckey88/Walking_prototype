50197140e99192544e41e28f2298110a8b7d5bb6
itsasam1 <mikeeaglesam@gmail.com>
2024-03-21 23:29:32 -0500

    got spawn points to spawn enemy prefabs
    going to have to go back through all the spawners though because I did not have the script added to any of the points :( this is only like 15 min of work but like :(
    also further testing could be nice.

---
+++ Walking_prototype/Assets/enemies/spawn_enemies.cs
@@ -0,0 +1,39 @@
+﻿//using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn_enemies : MonoBehaviour
+{
+    
+    public GameObject _enemyPrefab;
+
+    public float _minimumSpawnTime;
+
+    public float _maximumSpawnTime;
+
+    private float _timeUntilSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetTime();
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        _timeUntilSpawn -= Time.deltaTime;
+
+        if (_timeUntilSpawn <= 0) {
+
+            Instantiate(_enemyPrefab, transform.position, Quaternion.identity);
+            SetTime();
+        }
+    }
+
+    private void SetTime()
+    {
+        _timeUntilSpawn = UnityEngine.Random.Range(_minimumSpawnTime, _maximumSpawnTime);
+    }
+}
