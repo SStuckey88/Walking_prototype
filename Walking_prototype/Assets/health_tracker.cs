174d16da1d718b09da2cbc8e08cd91093f0a4246
ImSleep-Less <159849911+aexli2@users.noreply.github.com>
2024-03-24 23:31:46 -0500

    Added a Health bar
    I have edited the projects health tracker in the camera group to give the user a health stat and I have added a health bar in the form of a UI Slider and added 2 different sprites.

--- Walking_prototype/Assets/health_tracker.cs
+++ Walking_prototype/Assets/health_tracker.cs
@@ -1,18 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class health_tracker : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float currenthealth;
+    public float maxHealth=100;
+
+    public Slider mslider;
+
+
     void Start()
     {
-        
+        currenthealth=maxHealth;
+        mslider.maxValue = 100.0f;
+        mslider.minValue = 0.0f;
+        mslider.value = 100.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(10);
+        }
         
     }
-}
+
+    void TakeDamage(int damage)
+    {
+        currenthealth -= damage;
+        mslider.value -= 10.0f;
+    }
+
+}//End of Code
